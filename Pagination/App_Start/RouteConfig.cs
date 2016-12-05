b5f5c3c2cc8ce8f1c01052bb57535c6189c837a5
alex.paclin <alex.paclin@gmail.com>
2016-12-05 22:33:01 +0500

    Удобочитаемый маршрут

--- Pagination/App_Start/RouteConfig.cs
+++ Pagination/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Pagination
@@ -12,6 +8,12 @@ namespace Pagination
 		public static void RegisterRoutes(RouteCollection routes)
 		{
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute(
+				name: null,
+				url: "{controller}/{action}/{page}",
+				defaults: new { controller = "Home", action = "Index", page = UrlParameter.Optional }
+			);
 
 			routes.MapRoute(
 				name: "Default",

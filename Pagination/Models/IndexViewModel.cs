51f02a1f7304144687608b8f80927a6e202ad620
alex.paclin <alex.paclin@gmail.com>
2016-12-05 22:28:03 +0500

    Добавлена пагинация

---
+++ Pagination/Models/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Pagination.Models
+{
+	public class IndexViewModel
+	{
+		public IEnumerable<Phone> Phones { get; set; }
+		public PageInfo PageInfo { get; set; }
+	}
+}
